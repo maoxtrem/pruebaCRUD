80f0d1cc44bb6ad70554365187a61829b55947f5
maoxtrem <jmcastro1@misena.edu.co>
2021-11-20 09:44:30 -0500

    solicion a regex evitar espacios y caracteres especiales

--- src/Form/Type/ProductFormType.php
+++ src/Form/Type/ProductFormType.php
@@ -4,6 +4,7 @@ namespace App\Form\Type;
 
 use App\Entity\Category;
 use App\Entity\Product;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
@@ -18,12 +19,20 @@ class ProductFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('code', TextType::class,['required' => false])
-            ->add('name', TextType::class,['required' => false])
-            ->add('description', TextareaType::class,['required' => false])
-            ->add('brand', TextType::class,['required' => false])
-            ->add('category', EntityType::class, ['class' => Category::class, 'choice_label' => 'name','required' => false])
-            ->add('price', MoneyType::class,['required' => false])
+            ->add('code', TextType::class, ['required' => false])
+            ->add('name', TextType::class, ['required' => false])
+            ->add('description', TextareaType::class, ['required' => false])
+            ->add('brand', TextType::class, ['required' => false])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('C')
+                        ->where('C.active = 1');
+                },
+                'choice_label' => 'name',
+                'required' => false
+            ])
+            ->add('price', MoneyType::class, ['required' => false])
             ->add('guardar', SubmitType::class);
     }
 
